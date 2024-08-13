9efacc2341652f6e4106f5793e2ffd35a9316f57
the-gaurav-bhatt <bhattgaurav654@gmail.com>
2024-08-13 15:51:16 +0545

    Complete project setup

---
+++ server.ts
@@ -0,0 +1,25 @@
+import http from "http";
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose, { MongooseError } from "mongoose";
+const server = http.createServer(app);
+const URI = process.env.URI;
+console.log(URI);
+const PORT = process.env.PORT || 8000;
+const startServer = async () => {
+  if (URI) {
+    mongoose
+      .connect(URI)
+      .then(() => {
+        server.listen(PORT, () => {
+          console.log(`Server is running on port ${PORT}`);
+        });
+      })
+      .catch((err: MongooseError) => {
+        console.log(err);
+      });
+  } else console.log("Missing URI for MongoDB");
+};
+
+startServer();
